4063fd2759bcbbbc91e1da0808545a34ca2effeb
samanta1401 <psamanta1401@gmail.com>
2024-12-20 01:31:54 +0530

    dvc done

---
+++ stage_01.py
@@ -0,0 +1,5 @@
+with open('artifacts.txt', 'w') as f:
+    f.write('This is a test file for stage 01')
+
+with open('artifacts2.txt', 'w') as f:
+    f.write('This is a 2nd test file for stage 01')
---
+++ stage_02.py
@@ -0,0 +1,6 @@
+with open('artifacts.txt', 'r') as f:
+    text = f.read()
+
+print(f'stage 02  text:{text}')
+
+print('this is test change in stage 02')
